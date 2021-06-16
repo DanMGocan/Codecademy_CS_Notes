42d959c9e2d5683048e7903a429f849b3f54ca75
Dan Gocan <dangocan@outlook.com>
2021-06-16 03:13:32 +0100

    Added second lesson

--- commandline.py
+++ commandline.py
@@ -1,1 +1,8 @@
 # Codecademy_CS_Notes
+
+# ls - shows a list of all files
+# cd - change directory
+# pwd - print working directory
+# mkdir - create directory
+# touchon - create file
+# echo "Insert random text" >> text.txt - inserts text and creates the file
