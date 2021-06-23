0378cc149dbb38658189fff7230dc15e3a219d86
Dan Gocan <dangocan@outlook.com>
2021-06-23 01:01:19 +0100

    0101

---
+++ Git/commands.py
@@ -0,0 +1,7 @@
+git init # Initializes an empty git
+git status # displays information about state of git
+git add <filename> # adds file to the git repository
+### ^^^ this adds files to the staging area
+git diff <filename> # checks the differences for a file between working directory and staging area
+git commit -m "Message when committing"
+git log # allows us to see a log of all our versions and commits
--- Python/functions.py
+++ Python/functions.py
@@ -1,3 +1,31 @@
 def function_name():
     print("DEF is the keyword for a function")
 
+# help("str") is a handy function to give us information about functions and methods
+
+# Assign return to variable 
+def assigning_value(param1):
+    return param1 + param1
+
+double_value = assigning_value(74) # will now be equal to 148
+
+### MULTIPLE RETURNS
+def multiple_returns(param1, param2, param3):
+    monday = param1 + 10
+    tuesday = param2 + 20
+    friday = param3 * param3
+    return monday, tuesday, friday
+
+monday, tuesday, friday = multiple_returns(10, 20, 50)
+print(monday, tuesday, friday) # will print 20, 40, 2500
+
+#### str() can be used to convert integer and floate into string
+
+# lists (arrays) can be passed as arguments too
+def list_manipulation(lst):
+    print(lst[0])
+
+list_manipulation([1, 2, 3, 4]) #will print 1
+
+
+
