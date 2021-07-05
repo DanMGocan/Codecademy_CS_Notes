89094415f22129948e2baec7d2b2ef1190138eac
Dan Gocan <dangocan@outlook.com>
2021-07-05 08:18:26 +0100

    0818

--- Python/class.py
+++ Python/class.py
@@ -6,6 +6,7 @@ print(type(my_list)) # list
 
 class ClassName:
     def __init__(self, phrase):
+        self.motto = phrase # each instance gets created with a unique motto
         print(phrase)
 
     pass # we put pass here to mark it as an empty class and not get an Indentation Error
@@ -29,3 +30,25 @@ __function__ # <= this is a special / magic / dunder method
 cool_instance_2 = ClassName("Greetings!")
 # Greetings is passed to the __init__ method, which is the constructor
 # As a general rule is is best to initiate all class variables in the constructor method
+
+hasattr(object, "attribute") # returns True / False if object has / hasn't an attribute
+getattr(object, "attribute", default) # will get the value of an attribute 
+
+### SELF ###
+# We are isolating the class variables from instance variables
+# by creating instance variables with the constructor method
+class SearchEngineEntry:
+    secure_prefix = "https://"
+    def __init__(self, url):
+        self.url = url
+ 
+    def secure(self):
+        return "{prefix}{site}".format(prefix=self.secure_prefix, site=self.url)
+ 
+codecademy = SearchEngineEntry("www.codecademy.com")
+wikipedia = SearchEngineEntry("www.wikipedia.org")
+
+print(codecademy.secure())    
+print(wikipedia.secure())
+
+# We can use dir() to investigate an object's attributes 
